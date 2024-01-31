6724258f7e3661d47ab0f506ce3f3d59a6017162
MP Data - Trading <42841276+MPiccato@users.noreply.github.com>
2024-01-31 05:50:57 -0300

    Initial Commit

---
+++ SideMenuPractice/SideMenuPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuPracticeApp.swift
+//  SideMenuPractice
+//
+//  Created by Martin Piccato on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
